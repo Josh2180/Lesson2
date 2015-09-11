18759162d2331be61a27b63a1eee3b20171154e9
josh2180 <josh2180@ISS147D8-15.scdsb.pri>
2015-09-11 10:26:25 -0400

    Initial Commit

---
+++ src/Momentum.java
@@ -0,0 +1,17 @@
+import hsa.*;
+public class Momentum {
+
+    public static void main(String[] args) {
+        Console c=new Console();
+        double mass,velocity,momentum;
+        c.print("Enter object's mass: ");
+        mass=c.readDouble();
+        c.print("Enter object's velocity: ");
+        velocity=c.readDouble();
+        //calculate answer
+        momentum=mass*velocity;
+        //show result
+        c.print("The momentum is "+momentum);
+    }
+    
+}
